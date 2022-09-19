a9f2c575019efe8c4c2f4a199f285f4e66fc2247
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-09-19 19:30:21 +0600

    First Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    const getLengthStr = document.getElementById('length').value;
+    const getLength = parseFloat(getLengthStr);
+
+    const getWidthStr = document.getElementById('width').value;
+    const getWidth = parseFloat(getWidthStr);
+
+
+    
+})
+
+
+
